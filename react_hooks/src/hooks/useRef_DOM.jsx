1c2166e76019a2b74f09d3ce69abe18d4d327871
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2023-12-21 22:01:46 +0900

    useRef_DOM

---
+++ react_hooks/src/hooks/useRef_DOM.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef } from "react";
+
+export function UseRef_DOM() {
+  const inputRef = useRef();
+
+  useEffect(() => {
+    // console.log(inputRef);
+    inputRef.current.focus();
+  }, []);
+
+  const login = () => {
+    alert(`환영합니다 ${inputRef.current.value}!`);
+    inputRef.current.focus();
+  };
+
+  return (
+    <div>
+      <input ref={inputRef} type="text" placeholder="username" />
+      <button onClick={login}>로그인</button>
+    </div>
+  );
+}
+
+/** useRef를 통해 DOM요서에 직접적인 접근이 가능하다 */
