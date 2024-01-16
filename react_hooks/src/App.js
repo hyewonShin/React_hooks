03d8a734d852d3f57f2aa88007aab63c2048fa58
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-01-16 21:53:46 +0900

    useCallback2

--- react_hooks/src/App.js
+++ react_hooks/src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import UseMemo from "./hooks/useMemo";
 import UseMemo2 from "./hooks/useMemo2";
 import { UseCallback } from "./hooks/useCallback";
+import { UseCallback2 } from "./hooks/UseCallback2";
 
 function App() {
-  return <UseCallback />;
+  return <UseCallback2 />;
 }
 
 export default App;
